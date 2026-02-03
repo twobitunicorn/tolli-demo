c97b9ee9bcb968cd30ff407f449f7f37e2968a8d
twobitunicorn <joseph.ruskiewicz@gmail.com>
2026-02-03 10:59:22 -0800

    added some fun stuff.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import warnings
-from urllib.error import URLError
-
+import random
 import polars as pl
 import s3fs
 import streamlit as st
@@ -37,7 +36,9 @@ def get_contributors_data() -> pl.DataFrame:
     with s3.open(f"{BUCKET_NAME}/tolli/contributor_frame.ipc", "rb") as f:
         return pl.read_ipc_stream(f)
 
-st.markdown("""
+
+st.markdown(
+    """
 <style>
     /* Target the specific div that wraps the image and remove border-radius */
     .st-emotion-cache-1dp5vir { /* This class name is specific to Streamlit's internal structure and might change with updates */
@@ -49,7 +50,9 @@ st.markdown("""
         border-radius: 0px !important;
     }
 </style>
-""", unsafe_allow_html=True)
+""",
+    unsafe_allow_html=True,
+)
 
 
 st.set_page_config(page_title="Tolli.ai", page_icon="favicon.svg", layout="wide")
@@ -58,26 +61,50 @@ col_buffer_left, col_content, col_buffer_right = st.columns([1, 10, 1])
 
 
 team_frame = get_teams_data()
-contributors_frame = get_contributors_data()
+contributor_frame = get_contributors_data()
 
 with col_content:
     st.image("logoLight.svg")
-    title = st.title("Demo (v.0.0.11)")
-    _teams = {_team["slug"]: _team for _team in team_frame.rows(named=True)}
-    teams = st.multiselect(
-        label="Choose teams",
-        options=list(_teams.keys()),
-        default=list(_teams.keys()),
-        format_func=lambda x: _teams[x]["name"],
+    st.title("Demo (v.0.0.11)")
+    st.text(
+        "This is an industry overview as processed from various organizations.  All information is public.  This model is still in development and result should not be taken as fact."
     )
-    if not teams:
-        st.error("Please select at least one team.")
-    else:
-        for team in teams:
-            with st.container(horizontal=True):
-                st.image(_teams[team]["avatar_url"], width=68)
-                st.space("xxsmall")
-                st.subheader(_teams[team]["name"])
+    with st.container():
+        st.header("Clustering")
+        st.text(
+            "This is where we show the attributes of each team and engineer.  The tabs give some debug insight."
+        )
+
+        _teams = {_team["slug"]: _team for _team in team_frame.rows(named=True)}
+        teams = st.multiselect(
+            label="Choose teams",
+            options=list(_teams.keys()),
+            default=["microsoft", "facebook", "flutter"],
+            format_func=lambda x: _teams[x]["name"],
+        )
+        if not teams:
+            st.error("Please select at least one team.")
+        else:
+            for team in teams:
+                with st.container():
+                    with st.container(horizontal=True):
+                        st.image(_teams[team]["avatar_url"], width=68)
+                        st.space("xxsmall")
+                        st.subheader(_teams[team]["name"])
+                    trend_tab, variance_tab, cluster_tab = st.tabs(
+                        ["Trend", "Variance", "Clustering"]
+                    )
+                    with st.expander("Contributors"):
+                        for contributor in contributor_frame.filter(
+                            pl.col("current_team_slug") == team
+                        ).rows(named=True):
+                            with st.container(horizontal=True):
+                                st.space("xsmall")
+                                st.image(contributor["avatar_url"], width=42)
+                                st.space("xxsmall")
+                                st.text(
+                                    contributor["display_name"] or contributor["login"]
+                                )
 
             # for contributor in (
             #     get_contributors_data()
@@ -109,3 +136,24 @@ with col_content:
         #     )
         # )
         # st.altair_chart(chart, use_container_width=True)
+    with st.container():
+        st.header("Team Construction")
+        st.text(
+            "This is where we show the attributes of each team and engineer.  The tabs give some debug insight."
+        )
+
+        _contributors = {
+            _contributor["login"]: _contributor
+            for _contributor in contributor_frame.rows(named=True)
+        }
+        contributors = st.multiselect(
+            label="Choose contributors",
+            options=list(_contributors.keys()),
+            default=random.sample(list(_contributors.keys()), 7),
+            format_func=lambda x: _contributors[x]["display_name"]
+            or _contributors[x]["login"],
+        )
+        if not contributors:
+            st.error("Please select at least one contributors.")
+        else:
+            pass
