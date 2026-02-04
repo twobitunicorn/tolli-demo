56fa2b4d14e2a00e378c42cabec602d387dad946
twobitunicorn <joseph.ruskiewicz@gmail.com>
2026-02-04 11:52:41 -0800

    Removed the lazy expander.

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,10 @@ import polars as pl
 import s3fs
 import streamlit as st
 from dotenv import load_dotenv
+import polars.selectors as cs
 
+import streamlit as st
+        
 AWS_ACCESS_KEY_ID = "tid_xpRCdsbJTvJoNOJzygerAAVUDPlKVvGwGOCLwyuPvsZUnBnrEC"
 AWS_SECRET_ACCESS_KEY = (
     "tsec_lxcWdBpInYBlBfNw+kz7ySYDm+u+AsJW3eVGypI7Q3BUeyX8RfPPg2vFHO5jzI89XrvXJb"
@@ -24,6 +27,21 @@ warnings.filterwarnings("ignore")
 
 load_dotenv()
 
+aggs: list[pl.Expr] = [
+    pl.col("additions").mean(),
+    pl.col("changed_files").mean(),
+    pl.col("deletions").mean(),
+    pl.col("pull_count").mean(),
+    pl.col("merged_duration").mean(),
+    pl.col("closed_duration").mean(),
+    pl.col("pull_duration").mean(),
+    pl.col("closed").mean(),
+    pl.col("merged").mean(),
+    # pl.col("mergeable").mean()
+    pl.col("commit_count").mean(),
+    # pl.col("number").alias("pull_numbers")
+]
+
 
 @st.cache_data
 def get_teams_data() -> pl.DataFrame:
@@ -36,6 +54,16 @@ def get_contributors_data() -> pl.DataFrame:
     with s3.open(f"{BUCKET_NAME}/tolli/contributor_frame.ipc", "rb") as f:
         return pl.read_ipc_stream(f)
 
+
+@st.cache_data
+def get_trends_data() -> pl.DataFrame:
+    with s3.open(f"{BUCKET_NAME}/tolli/trend_frame.ipc", "rb") as f:
+        return pl.read_ipc_stream(f)
+
+@st.cache_data
+def get_variances_data() -> pl.DataFrame:
+    with s3.open(f"{BUCKET_NAME}/tolli/variance_frame.ipc", "rb") as f:
+        return pl.read_ipc_stream(f)
 
 st.markdown(
     """
@@ -62,6 +90,23 @@ col_buffer_left, col_content, col_buffer_right = st.columns([1, 10, 1])
 
 team_frame = get_teams_data()
 contributor_frame = get_contributors_data()
+trend_frame = get_trends_data()
+org_trend_frame = trend_frame.sort("org_login", "date").group_by_dynamic(
+    "date", every="1d", start_by="monday", group_by=["org_login"]
+).agg(aggs)
+
+team_trend_frame = trend_frame.sort("org_login", "date").group_by_dynamic(
+    "date", every="1d", start_by="monday", group_by=["org_login", "team_slug"]
+).agg(aggs)
+
+variance_frame = get_variances_data()
+org_variance_frame = variance_frame.sort("org_login", "date").group_by_dynamic(
+    "date", every="1d", start_by="monday", group_by=["org_login"]
+).agg(aggs)
+
+team_variance_frame = variance_frame.sort("org_login", "date").group_by_dynamic(
+    "date", every="1d", start_by="monday", group_by=["org_login", "team_slug"]
+).agg(aggs)
 
 with col_content:
     st.image("logoLight.svg")
@@ -70,12 +115,27 @@ with col_content:
         "This is an industry overview as processed from various organizations.  All information is public.  This model is still in development and result should not be taken as fact."
     )
     with st.container():
+        with st.expander("Debug Info"):
+            trend_tab, variance_tab, cluster_tab = st.tabs(
+                ["Trend", "Variance", "Clustering"]
+            )
+            with trend_tab:
+                st.altair_chart(
+                    org_trend_frame.filter(pl.col("org_login") == "tolli-inc").unpivot(cs.numeric(), index=["org_login", "date"], variable_name="data_point", value_name="value").plot.line(x="date:T", y="value:Q", color="data_point:N")
+                )
+            with variance_tab:
+                st.altair_chart(
+                    org_variance_frame.filter(pl.col("org_login") == "tolli-inc").unpivot(cs.numeric(), index=["org_login", "date"], variable_name="data_point", value_name="value").plot.line(x="date:T", y="value:Q", color="data_point:N")
+                )
+
+    with st.container():
         st.header("Clustering")
         st.text(
             "This is where we show the attributes of each team and engineer.  The tabs give some debug insight."
         )
 
         _teams = {_team["slug"]: _team for _team in team_frame.rows(named=True)}
+
         teams = st.multiselect(
             label="Choose teams",
             options=list(_teams.keys()),
@@ -91,9 +151,7 @@ with col_content:
                         st.image(_teams[team]["avatar_url"], width=68)
                         st.space("xxsmall")
                         st.subheader(_teams[team]["name"])
-                    trend_tab, variance_tab, cluster_tab = st.tabs(
-                        ["Trend", "Variance", "Clustering"]
-                    )
+                    
                     with st.expander("Contributors"):
                         for contributor in contributor_frame.filter(
                             pl.col("current_team_slug") == team
@@ -105,6 +163,18 @@ with col_content:
                                 st.text(
                                     contributor["display_name"] or contributor["login"]
                                 )
+                    with st.expander("Debug Info"):
+                        trend_tab, variance_tab, cluster_tab = st.tabs(
+                            ["Trend", "Variance", "Clustering"]
+                        )
+                        with trend_tab:
+                            st.altair_chart(
+                                team_trend_frame.filter((pl.col("org_login") == "tolli-inc") & (pl.col("team_slug") == team)).unpivot(cs.numeric(), index=["org_login", "date"], variable_name="data_point", value_name="value").plot.line(x="date:T", y="value:Q", color="data_point:N")
+                            )
+                        with variance_tab:
+                            st.altair_chart(
+                                team_variance_frame.filter((pl.col("org_login") == "tolli-inc") & (pl.col("team_slug") == team)).unpivot(cs.numeric(), index=["org_login", "date"], variable_name="data_point", value_name="value").plot.line(x="date:T", y="value:Q", color="data_point:N")
+                            )
 
             # for contributor in (
             #     get_contributors_data()
@@ -150,8 +220,9 @@ with col_content:
             label="Choose contributors",
             options=list(_contributors.keys()),
             default=random.sample(list(_contributors.keys()), 7),
-            format_func=lambda x: _contributors[x]["display_name"]
-            or _contributors[x]["login"],
+            format_func=lambda x: (
+                _contributors[x]["display_name"] or _contributors[x]["login"]
+            ),
         )
         if not contributors:
             st.error("Please select at least one contributors.")
